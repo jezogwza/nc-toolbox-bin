c111d6b4b7ade924ba96243d14689787b670b316
Rodolfo <jezogwza@gmail.com>
2024-03-25 13:04:12 -0400

    Initialized security.go

--- pkg/users/security.go
+++ pkg/users/security.go
@@ -4,3 +4,17 @@ import (
 	"fmt"
 	"os/user"
 )
+
+/** Only allow users with sudo priviliges */
+func HavePriviliges() bool {
+	u, err := user.Current()
+	if err != nil {
+		fmt.Println("Error getting current user")
+		return false
+	}
+	// @@TODO - SHould check if teh user has access to sudo
+	if u.Username == "root" {
+		return true
+	}
+	return false
+}
